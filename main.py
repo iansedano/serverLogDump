ed20f1249c249fba6ae519491d203871d0dab6fc
Ian <uer@zu1.l.hostens.cloud>
2021-10-12 13:20:16 +0300

    successfully parsing logs

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+"""
+sudo -E env PATH=$PATH python3 main.py
+"""
+
 import json
 import pathlib
 
+from apachelogs import LogParser, COMBINED
+
+parser = LogParser(COMBINED)
+
 log_path = pathlib.Path("/var/log/apache2/flaskrest-access.log.1")
 
-contents = log_path.read_text()
-
-lines = contents.split("\n")
-
-for i, l in enumerate(lines):
-	parts = l.split(" ")
-	for j, p in enumerate(parts):
-		print(i, j, p)
+with open(log_path) as fp:
+	for entry in parser.parse_lines(fp):
+		print(str(entry.request_time), entry.request_line)
+	
