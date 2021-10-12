0c26167d195617727fc1fa0edc28174ed556821c
Ian <iansedano@gmail.com>
2021-10-12 13:36:27 +0300

    getting fields

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!venv/bin/python
 """
 sudo -E env PATH=$PATH python3 main.py
 """
@@ -5,13 +6,19 @@ sudo -E env PATH=$PATH python3 main.py
 import json
 import pathlib
 
-from apachelogs import LogParser, COMBINED
+from apachelogs import LogParser
 
-parser = LogParser(COMBINED)
+parser = LogParser("%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"")
 
 log_path = pathlib.Path("/var/log/apache2/flaskrest-access.log.1")
 
 with open(log_path) as fp:
 	for entry in parser.parse_lines(fp):
-		print(str(entry.request_time), entry.request_line)
-	
+		print(
+			entry.remote_host,
+			entry.request_time_fields["timestamp"],
+			entry.request_line,
+			entry.final_status,
+			entry.headers_in["Referer"],
+			entry.headers_in["User-Agent"]
+		)
